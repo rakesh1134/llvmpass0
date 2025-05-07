80cf98df8ca679a84b95fa636450d94623a1f67c
INFOR\rthelkap <rakesh.thelkapally@infor.com>
2025-05-07 23:59:37 +0530

    add calc program

---
+++ calc.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+        return a + b;
+}
+
+int sub(int a, int b)
+{
+        return a - b;
+}
+
+int prd(int a, int b)
+{
+        return a * b;
+}
+
+
+int main () {
+        cout << "Hello World" <<endl;
+        return 0;
+}
